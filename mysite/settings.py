0efb3946b89e732d6b2aa3dfe6aa9a5560908e37
Nandani Dalsaniya <nandanidalsaniya@gmail.com>
2020-10-02 18:10:27 +0530

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\admin\PycharmProjects\pythonProject\djangogirls\blog\tempalets'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
